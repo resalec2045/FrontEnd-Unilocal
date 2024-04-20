c6ff0f2ae5d369db9ef910ef3149621e649847d4
Andres <andreciito38@gmail.com>
2024-04-19 21:19:44 -0500

    Clase 19/04/2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: '**', pathMatch: "full", redirectTo: "" },
+];
