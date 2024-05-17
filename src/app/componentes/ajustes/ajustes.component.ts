946098b3bb73214d7f94dab8afb65fbe2b05c2c2
David Velasco <david.santiago.velasco.12@gmail.com>
2024-05-17 16:17:58 -0500

    ajustes: botones funcionales

--- src/app/componentes/ajustes/ajustes.component.ts
+++ src/app/componentes/ajustes/ajustes.component.ts
@@ -1,14 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavBarComponent } from '../generales/navbar/navbar.component';
 import { FooterComponent } from '../generales/footer/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { TokenService } from '../../services/token.service';
+import { ClienteService } from '../../services/cliente.service';
+import { ImagenesService } from '../../services/imagenes.service';
+import { ClienteActualizadoDTO } from '../../dto/clienteActualizadoDTO';
 
 @Component({
   selector: 'app-ajustes',
   standalone: true,
-  imports: [ NavBarComponent, FooterComponent],
+  imports: [ NavBarComponent, FooterComponent, FormsModule],
   templateUrl: './ajustes.component.html',
   styleUrl: './ajustes.component.css'
 })
 export class AjustesComponent {
+
+  clienteActualizadoDTO: ClienteActualizadoDTO;
+  constructor(private tokenService: TokenService,private clienteService: ClienteService,private imagenesService: ImagenesService) {
+    this.clienteActualizadoDTO = new ClienteActualizadoDTO();
+  }
+
+  ngOnInit(): void {
+    this.obtenerCliente();
+  }
+
+  public guardarCambios() {
+
+    this.insertarImagen();
+
+    // this.clienteService.actualizarCliente(this.clienteActualizadoDTO).subscribe(
+    //   {
+    //     next: (data) => {
+    //       console.log(data);
+    //     },
+    //     error: (error) => {
+    //       console.log(error);
+    //     }
+    //   }
+    // );
+
+  }
+
+  public insertarImagen() {
+    this.imagenesService.subirImagen(this.clienteActualizadoDTO.fotoActualizada).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    );
+  }
+
+  public obtenerCliente() {
+    const { id } = this.tokenService.decodePayload();
+    this.clienteService.obtenerCliente(id).subscribe(
+      {
+        next: (data) => {
+          this.clienteActualizadoDTO = data.respuesta;
+          console.log(this.clienteActualizadoDTO);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    
+    );
+  }
+  
+  isFieldEnabledNombre: boolean = false;
+  isFieldEnabledApodo: boolean = false;
+  isFieldEnabledCorreo: boolean = false;
+  isFieldEnabledCiudad: boolean = false;
+
+  habilitarEdicionNombre() {
+    this.isFieldEnabledNombre = !this.isFieldEnabledNombre;
+  }
+
+  habilitarEdicionApodo() {
+    this.isFieldEnabledApodo = !this.isFieldEnabledApodo;
+  }
+
+  habilitarEdicionCorreo() {
+    this.isFieldEnabledCorreo = !this.isFieldEnabledCorreo;
+  }
+
+  habilitarEdicionCiudad() {
+    this.isFieldEnabledCiudad = !this.isFieldEnabledCiudad;
+  }
+
 
 }
